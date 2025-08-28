8e3b3937f396b0cce1d3cb2b05d205649f690821
atielijohn8 <atielijohn4@gmail.com>
2025-08-29 01:12:32 +0300

    update

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,28 @@
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(engine)#  Creates the database tables using SQLAlchemy's metadata
 
 def save(session, dog):
-    pass
+    session.add(dog) #adds dog instance 
+    session.commit() # commits to a db
 
 def get_all(session):
-    pass
+    return session.query(Dog).all() #returns all dog recods from db
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name=name).first() #finds by name
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id=id).first() #finds dog ID
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(name=name, breed=breed).first() #finds dog by name and breed
+
+def update_name(session, dog, name):
+    dog.name = name #updates name and commit changes
+    session.commit()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
